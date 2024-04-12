9121cda6233517d613351f0624df2bdd001746ea
Shubham <shubhampawar4036@gmail.com>
2024-04-12 20:46:09 +0530

    ok

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,9 @@ const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 dotenv.config();
+app.get("/",(req,res) => {
+	res.json("Hello");
+	})
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
