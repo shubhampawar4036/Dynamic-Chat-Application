6ecf05710b7d35398e1e4b6acac2761c94e4ed82
Shubham <shubhampawar4036@gmail.com>
2024-04-13 10:06:22 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -15,24 +15,33 @@ const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 dotenv.config();
-app.get("/",(req,res) => {
-	res.json("Hello");
-	})
 
-app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
-app.use(cookieParser());
+async function startServer() {
+	try {
 
-app.use("/api/auth", authRoutes);
-app.use("/api/messages", messageRoutes);
-app.use("/api/users", userRoutes);
-
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-});
-
-server.listen(PORT, () => {
-	connectToMongoDB();
-	console.log(`Server Running on port ${PORT}`);
-});
+		app.get("/",(req,res) => {
+			res.json("Hello");
+			})
+		
+		app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+		app.use(cookieParser());
+		
+		app.use("/api/auth", authRoutes);
+		app.use("/api/messages", messageRoutes);
+		app.use("/api/users", userRoutes);
+		
+		app.use(express.static(path.join(__dirname, "/frontend/dist")));
+		
+		app.get("*", (req, res) => {
+			res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+		});
+		
+		server.listen(PORT, () => {
+			connectToMongoDB();
+			console.log(`Server Running on port ${PORT}`);
+		});
+	}catch(err) {
+		console.log("error starting server",err);
+	}
+}
+startServer();
