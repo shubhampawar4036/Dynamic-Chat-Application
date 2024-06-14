20fb7bb02a44fa9d96e8501a7aed97dfea997b9c
Shubham <shubhampawar4036@gmail.com>
2024-06-14 11:59:45 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -16,32 +16,20 @@ const __dirname = path.resolve();
 
 dotenv.config();
 
-async function startServer() {
-	try {
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+app.use(cookieParser());
 
-		app.get("/",(req,res) => {
-			res.json("Hello");
-			})
-		
-		app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
-		app.use(cookieParser());
-		
-		app.use("/api/auth", authRoutes);
-		app.use("/api/messages", messageRoutes);
-		app.use("/api/users", userRoutes);
-		
-		app.use(express.static(path.join(__dirname, "/frontend/dist")));
-		
-		app.get("*", (req, res) => {
-			res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-		});
-		
-		server.listen(PORT, () => {
-			connectToMongoDB();
-			console.log(`Server Running on port ${PORT}`);
-		});
-	}catch(err) {
-		console.log("error starting server",err);
-	}
-}
-startServer();
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+server.listen(PORT, () => {
+	connectToMongoDB();
+	console.log(`Server Running on port ${PORT}`);
+});
--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-yt.onrender.com", {
+			const socket = io("https://chat-app-server-two-taupe.vercel.app", {
 				query: {
 					userId: authUser._id,
 				},
